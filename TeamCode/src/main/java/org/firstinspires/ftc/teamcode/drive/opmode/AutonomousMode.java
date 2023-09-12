46d7d99a9cc831c058558664ed5fa43d113f1cea
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-09-12 10:00:59 -0400

    detection stuff
    stuff i found out about team piece plus detection

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonomousMode.java
@@ -15,6 +15,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
+import java.util.*;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
@@ -30,6 +31,10 @@ public class AutonomousMode extends LinearOpMode {
     AprilTagProcessor.Builder aprilTagProcessorBuilder;
 
     SampleMecanumDrive drive;
+    int targetTag;
+    double detX; //goal 0, means looking straight at tag
+    double detY; // goal ?? depends per robot
+    double targetY = 12;
 
     @Override
     public void runOpMode() throws InterruptedException {
@@ -45,6 +50,58 @@ public class AutonomousMode extends LinearOpMode {
         vPortal = vPortalBuilder.build();
 
         drive = new SampleMecanumDrive(hardwareMap);
+
+        // the team piece has to be entirely red or blue except for letters, and letters cant be greater than 0.5 in
+        // shades of red? shiny/dull red? or make distinct red/blue colored outlinen that's very recognizable
+        // may need to code own processor, like in FF (see: VisionProcessor -> VisionProccesorInternal, similar structure)
+        // tags different for each side
+
+        waitForStart();
+
+        if(opModeIsActive()) {
+            while(opModeIsActive()) {
+
+
+                List<AprilTagDetection> allDets = aprilTagProcessor.getDetections();
+                for (AprilTagDetection det : allDets) {
+
+                    // targetTag isolated
+                    if (det.id % 3 == targetTag || det.id % 3 == targetTag-3) {
+                        detX = det.ftcPose.x;
+                        detY = det.ftcPose.y;
+
+                        // assumign using sample mecanum drive, RR
+                        //assuming robot made a perfect 90 deg turn and is just to the right or left of tag
+                        while(detX !=0) {
+                            if (detX > 0) {
+                                drive.setMotorPowers(detX/10,-detX/10,detX/10,-detX/10);
+                            } else {
+                                drive.setMotorPowers(-detX/10,detX/10,-detX/10,detX/10);
+                            }
+                            this.sleep(20);
+                        }
+
+
+                        while(detY != targetY) {
+                            if (detX > targetY) {
+                                drive.setMotorPowers(0.1,0.1,0.1,0.1); // do something about this later
+                            } else {
+                                drive.setMotorPowers(-0.1,-0.1,-0.1,-0.1);
+                            }
+                            this.sleep(20);
+
+                        }
+
+                        // lol this isn't gonna work
+                        break;
+                    }
+                }
+
+
+
+
+            }
+        }
 
 
 
