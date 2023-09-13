0efee51ffa7a7684c262eb811832ff0018ecf898
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-09-13 14:51:48 -0400

    added stuff for robot direction from april tags
    use camTest2023
    it will say which direction from the center of the camera's view the target tag is (in this case its only doing left)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonomousMode.java
@@ -33,23 +33,27 @@ public class AutonomousMode extends LinearOpMode {
     AprilTagProcessor.Builder aprilTagProcessorBuilder;
 
     SampleMecanumDrive drive;
-    int targetTag;
+    int targetTag = 1;
     double detX; //goal 0, means looking straight at tag
     double detY; // goal ?? depends per robot
     double targetY = 12;
+    String direction;
+    String strafe;
 
     @Override
     public void runOpMode() throws InterruptedException {
         aprilTagProcessor = initAprilTag();
         vPortal = initVisionPortal(aprilTagProcessor);
 
-        drive = new SampleMecanumDrive(hardwareMap);
+//        drive = new SampleMecanumDrive(hardwareMap);
 
         waitForStart();
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
 //                May be blocking!!!
+                aprilTagTelemetry();
+                telemetry.update();
                 List<AprilTagDetection> aprilTagDetectionsList = aprilTagProcessor.getDetections();
             }
 
@@ -88,12 +92,53 @@ public class AutonomousMode extends LinearOpMode {
                     telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
                     telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
                     telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+
+
+                        // targetTag isolated
+                        if (detection.id % 3 == targetTag || detection.id % 3 == targetTag-3) {
+                            detX = detection.ftcPose.x;
+                            detY = detection.ftcPose.y;
+
+                            // assumign using sample mecanum drive, RR
+                            //assuming robot made a perfect 90 deg turn and is just to the right or left of tag
+                            if(detX != 0) {
+                                if (detX > 0) {
+//                                    drive.setMotorPowers(detX/10,-detX/10,detX/10,-detX/10);
+                                    strafe = "right";
+                                } else {
+//                                    drive.setMotorPowers(-detX/10,detX/10,-detX/10,detX/10);
+                                    strafe = "left";
+                                }
+
+//                                this.sleep(20);
+                            }
+
+
+                            if(detY!=targetY) {
+                                if (detY > targetY) {
+                                     direction = "forward";
+//                                    drive.setMotorPowers(0.1,0.1,0.1,0.1); // do something about this later
+                                } else {
+//                                    drive.setMotorPowers(-0.1,-0.1,-0.1,-0.1);
+                                     direction = "backward";
+                                }
+//                                this.sleep(20);
+
+                            }
+                        }
+
+
+
+
+
+
                 } else {
                     telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
                     telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
                 }
             }   // end for() loop
-
+            telemetry.addData("X direction: ", strafe);
+            telemetry.addData("Y direction: ", direction);
             // Add "key" information to telemetry
             telemetry.addLine("\nkey:\nXYZ = X (Right), Y (Forward), Z (Up) dist.");
             telemetry.addLine("PRY = Pitch, Roll & Yaw (XYZ Rotation)");
