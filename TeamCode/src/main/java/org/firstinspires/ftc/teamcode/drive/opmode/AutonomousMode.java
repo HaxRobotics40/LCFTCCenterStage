6f32a4c06a060fddf9c31e21673616b81096a0eb
Max Lazarre <maxlazarr@gmail.com>
2023-09-15 16:01:25 -0400

    Async Telemetry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonomousMode.java
@@ -2,13 +2,9 @@ package org.firstinspires.ftc.teamcode.drive.opmode;
 
 import android.annotation.SuppressLint;
 
-import com.acmerobotics.roadrunner.geometry.Pose2d;
-import com.acmerobotics.roadrunner.geometry.Vector2d;
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
@@ -16,17 +12,11 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AngularVelocity;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
-import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
-import org.firstinspires.ftc.vision.apriltag.AprilTagLibrary;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import java.util.*;
-import org.openftc.easyopencv.OpenCvCamera;
-import org.openftc.easyopencv.OpenCvCameraFactory;
-import org.openftc.easyopencv.OpenCvCameraRotation;
-
 
 
 @Autonomous
@@ -55,7 +45,7 @@ public class AutonomousMode extends LinearOpMode {
                 while (!Thread.currentThread().isInterrupted() && opModeIsActive()) {
                     outputTelemetry();
                     try {
-                        Thread.sleep(50); // Introducing a small delay to prevent excessive updates
+                        Thread.sleep(10); // Introducing a small delay to prevent excessive updates
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                     }
@@ -69,12 +59,15 @@ public class AutonomousMode extends LinearOpMode {
 
         if (opModeIsActive()) {
             while (opModeIsActive()) {
-//                May be blocking!!!
-                List<AprilTagDetection> aprilTagDetectionsList = aprilTagProcessor.getDetections();
+//               opModeLoop();
             }
         }
 
         telemetryThread.interrupt(); // Make sure to interrupt the telemetry thread when opMode is no longer active
+    }
+
+    private void opModeLoop() {
+
     }
 
     private AprilTagProcessor initAprilTag() {
@@ -101,8 +94,18 @@ public class AutonomousMode extends LinearOpMode {
     }
 
     private void outputTelemetry() {
+        // TODO: Also output to .log file.
+        telemetry.addLine("---------April Tag Data----------");
         aprilTagTelemetry();
+        telemetry.addLine("---------IMU Data----------");
         IMUTelemetry();
+        telemetry.addLine("---------Pose Data----------");
+//        TODO: Add beysian estimate. Kalman filter.
+        poseTelemetry();
+    }
+
+    private void poseTelemetry() {
+//        telemetry.addLine(String.format("Estimated Pose: %s", drive.getPoseEstimate().toString()));
     }
 
     @SuppressLint("DefaultLocale")
@@ -112,10 +115,15 @@ public class AutonomousMode extends LinearOpMode {
 
             // Step through the list of detections and display info for each one.
             for (AprilTagDetection detection : currentDetections) {
-                telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                if (detection.metadata != null) {
+                    telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                    telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                    telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                    telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                } else {
+                    telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                    telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                }
             }   // end for() loop
     }
 
