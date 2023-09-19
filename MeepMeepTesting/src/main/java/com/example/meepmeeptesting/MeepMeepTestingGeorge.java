e6fdf1c887aa8b827ceede8b22885770df3a36bc
geormirg <geormirg@gmail.com>
2023-09-19 16:39:18 -0400

    messing with my meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingGeorge.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTestingGeorge.java
@@ -19,14 +19,14 @@ public class MeepMeepTestingGeorge {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 0))
-                                .forward(30)
+                        drive.trajectorySequenceBuilder(new Pose2d(0, 0, 30))
+                                .forward(40)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
+                                .forward(40)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
+                                .forward(40)
                                 .turn(Math.toRadians(90))
-                                .forward(30)
+                                .forward(40)
                                 .turn(Math.toRadians(90))
                                 .build()
                 );
