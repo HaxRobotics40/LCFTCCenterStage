69a7a1e3a35f36144268b68fe79b20304687da11
soggy-cereal1 <114190885+soggy-cereal1@users.noreply.github.com>
2023-09-26 10:36:30 -0400

    visionprocessors or opencvpipelines?
    they use different drawing libraries for some reason :( why android.graphics (let's use opencvpipeline this is a waste of time?)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/vision/testVisionProcessor.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/vision/testVisionProcessor.java
@@ -1,31 +1,42 @@
 package org.firstinspires.ftc.teamcode.drive.opmode.vision;
 
-import org.opencv.core.*;
-import org.opencv.core.Mat;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-import org.opencv.photo.Photo;
-import org.openftc.easyopencv.OpenCvPipeline;
-import org.firstinspires.ftc.robotcore.external.Telemetry;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Color;
+import android.graphics.Rect;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class testEOCVpipeline extends OpenCvPipeline {
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.internal.camera.calibration.CameraCalibration;
+import org.firstinspires.ftc.vision.VisionProcessor;
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.*;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.photo.Photo;
+
+public class testVisionProcessor implements VisionProcessor {
     // TODO: make this a vision processor later, it's the exact same thing (see: LJ4FTC)
     Mat secondary = new Mat();
+    Mat edges = new Mat();
     int location;
+    Rect rect = new Rect(20, 20, 50, 50);
     final int width = 900; // TODO: add as constructor parameter when done testing
     int color;
 
+    public void init(int width, int height, CameraCalibration calibration) {
 
+    }
     Telemetry telemetry; //TODO: get rid of all instances of Telemtry as an object
-    public testEOCVpipeline(Telemetry telemetry) {
+    public testVisionProcessor(Telemetry telemetry) {
         this.telemetry = telemetry;
     }
 
 
-    public Mat processFrame(Mat input) {
+    public Mat processFrame(Mat input, long captureTimeNanos) {
 
         Imgproc.cvtColor(input, secondary, Imgproc.COLOR_RGB2HSV);
 
@@ -56,7 +67,6 @@ public class testEOCVpipeline extends OpenCvPipeline {
         Mat maskR2 = new Mat();
         Mat colorOnly = new Mat();
         Mat mask = new Mat();
-        Mat edges = new Mat();
         int amountColor;
         int type = colorOnly.type();
 
@@ -70,6 +80,7 @@ public class testEOCVpipeline extends OpenCvPipeline {
         Core.bitwise_and(maskR, maskR, mask, maskR = maskR);
         Core.bitwise_and(maskB, maskB, mask, maskB = maskB);
         Core.bitwise_and(maskR2, maskR2, mask, maskR2 = maskR2);
+        secondary.release();
         maskR.release();
         maskB.release();
         maskR2.release();
@@ -82,39 +93,43 @@ public class testEOCVpipeline extends OpenCvPipeline {
         // edges = mask.clone();
 
         // Imgproc.cvtColor(mask, edges, Imgproc.COLOR_RGB2GRAY);
-
-
-        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Mat hierarchy = new Mat();
-        Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
-
-        for (int i=0; i<contours.size(); i++) {
-            //Convert contours(i) from MatOfPoint to MatOfPoint2f
-            MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
-            //Processing on mMOP2f1 which is in type MatOfPoint2f
-            double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
-            Imgproc.approxPolyDP(contour2f, contour2f, approxDistance, true);
-
-            //Convert back to MatOfPoint
-            MatOfPoint points = new MatOfPoint( contour2f.toArray() );
-
-            // Get bounding rect of contour
-            Rect rect = Imgproc.boundingRect(points);
-
-            // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
-            Imgproc.rectangle(edges, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(225,0,225));
-
-        }
-
-        double midline = 0.5 * width;
-
         // figure out how to see where the largest boxes are
+        // List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+        // Mat hierarchy = new Mat();
+        // Imgproc.findContours(edges, contours, hierarchy, Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+
+        // for (int i=0; i<contours.size(); i++) {
+        //     //Convert contours(i) from MatOfPoint to MatOfPoint2f
+        //     MatOfPoint2f contour2f = new MatOfPoint2f(contours.get(i).toArray());
+        //     //Processing on mMOP2f1 which is in type MatOfPoint2f
+        //     double approxDistance = Imgproc.arcLength(contour2f, true)*0.02;
+        //     Imgproc.approxPolyDP(contour2f, contour2f, approxDistance, true);
+
+        //     //Convert back to MatOfPoint
+        //     MatOfPoint points = new MatOfPoint( contour2f.toArray() );
+
+        //     // Get bounding rect of contour
+        //     rect = Imgproc.boundingRect(points);
+
+        //     // draw enclosing rectangle (all same color, but you could use variable i to make them unique)
+        //     Imgproc.rectangle(edges, new Point(rect.x,rect.y), new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(225,0,225));
+
+        // }
 
         // comment this out when implementing on the bot
         telemetry.addData("Location", location);
         telemetry.update();
 
+
         return edges;
+    }
+
+    public void onDrawFrame(Canvas canvas, int onscreenWidth, int onscreenHeight, float scaleBmpPxToCanvasPx, float scaleCanvasDensity, Object userContext) {
+
+        Paint selectedPaint = new Paint();
+        selectedPaint.setColor(Color.RED);
+        selectedPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawRect(rect, selectedPaint);
     }
 
     public int pieceLocation() {
