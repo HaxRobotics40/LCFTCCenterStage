773e03edfbe22bf4508ec6d9d91af8f24510e7b1
Dylanqw <145394726+Dylanqw@users.noreply.github.com>
2023-10-03 16:46:35 -0400

    summary (required)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonomousMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/opmode/AutonomousMode.java
@@ -23,6 +23,7 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagGameDatabase;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import java.util.*;
+import com.qualcomm.robotcore.util.RobotLog;
 
 
 @Autonomous
@@ -33,7 +34,7 @@ public class AutonomousMode extends LinearOpMode {
     AprilTagProcessor aprilTagProcessor;
     AprilTagProcessor.Builder aprilTagProcessorBuilder;
 
-    SampleMecanumDrive drive;
+//    SampleMecanumDrive drive;
 //    TODO: Use dead wheels
 //    TODO: Update Constants to be 100% accurate (ex. wheel radius)
     IMU imu;
@@ -44,15 +45,15 @@ public class AutonomousMode extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
-        aprilTagProcessor = initAprilTag();
-        vPortal = initVisionPortal(aprilTagProcessor);
+//        aprilTagProcessor = initAprilTag();
+//        vPortal = initVisionPortal(aprilTagProcessor);
 
         setupIMU();
 
 //        setupColorSensor();
         setupDistanceSensor();
 
-        drive = new SampleMecanumDrive(hardwareMap);
+//        drive = new SampleMecanumDrive(hardwareMap);
         // TODO: Fix Drive Constants physical measurements!!!
 //        TODO: Move Reverse to here.
 
@@ -94,13 +95,13 @@ public class AutonomousMode extends LinearOpMode {
         return aprilTagProcessorBuilder.build();
     }
 
-    private VisionPortal initVisionPortal(AprilTagProcessor atp) {
-        vPortalBuilder = new VisionPortal.Builder();
-        vPortalBuilder.setCamera(hardwareMap.get(WebcamName.class, "webcam"));
-        vPortalBuilder.addProcessor(atp);
-
-        return vPortalBuilder.build();
-    }
+//    private VisionPortal initVisionPortal(AprilTagProcessor atp) {
+//        vPortalBuilder = new VisionPortal.Builder();
+//        vPortalBuilder.setCamera(hardwareMap.get(WebcamName.class, "webcam"));
+//        vPortalBuilder.addProcessor(atp);
+//
+//        return vPortalBuilder.build();
+//    }
 
     private void setupIMU() {
         imu = hardwareMap.get(IMU.class, "imu");
@@ -128,50 +129,50 @@ public class AutonomousMode extends LinearOpMode {
 
     private void outputTelemetry() {
         // TODO: Also output to .log file.
-        telemetry.addLine("---------April Tag Data----------");
+        telemetryLine("---------April Tag Data----------");
         aprilTagTelemetry();
-        telemetry.addLine("---------IMU Data----------");
+        telemetryLine("---------IMU Data----------");
         IMUTelemetry();
-        telemetry.addLine("---------Pose Data----------");
+        telemetryLine("---------Pose Data----------");
 //        TODO: Add beysian estimate. Kalman filter.
         poseTelemetry();
-//        telemetry.addLine("---------Color Data----------");
+//        telemetryLine("---------Color Data----------");
 //        colorSensorTelemetry();
-        telemetry.addLine("---------Distance Sensor----------");
+        telemetryLine("---------Distance Sensor----------");
         distanceSensorTelemetry();
     }
 
     private void distanceSensorTelemetry() {
-        telemetry.addData("range", String.format("%.01f mm", sensorDistance.getDistance(DistanceUnit.MM)));
+        telemetryData("range", String.format("%.01f mm", sensorDistance.getDistance(DistanceUnit.MM)));
     }
 
 //    private void colorSensorTelemetry() {
 //        NormalizedRGBA colors = colorSensor.getNormalizedColors();
-//        telemetry.addLine()
+//        telemetryLine()
 //                .addData("Red", "%.3f", colors.red)
 //                .addData("Green", "%.3f", colors.green)
 //                .addData("Blue", "%.3f", colors.blue);
 //    }
 
     private void poseTelemetry() {
-//        telemetry.addLine(String.format("Estimated Pose: %s", drive.getPoseEstimate().toString()));
+//        telemetryLine(String.format("Estimated Pose: %s", drive.getPoseEstimate().toString()));
     }
 
     @SuppressLint("DefaultLocale")
     private void aprilTagTelemetry() {
             List<AprilTagDetection> currentDetections = aprilTagProcessor.getDetections();
-            telemetry.addData("# AprilTags Detected", currentDetections.size());
+            telemetryData("# AprilTags Detected", currentDetections.size());
 
             // Step through the list of detections and display info for each one.
             for (AprilTagDetection detection : currentDetections) {
                 if (detection.metadata != null) {
-                    telemetry.addLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
-                    telemetry.addLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
-                    telemetry.addLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
-                    telemetry.addLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
+                    telemetryLine(String.format("\n==== (ID %d) %s", detection.id, detection.metadata.name));
+                    telemetryLine(String.format("XYZ %6.1f %6.1f %6.1f  (inch)", detection.ftcPose.x, detection.ftcPose.y, detection.ftcPose.z));
+                    telemetryLine(String.format("PRY %6.1f %6.1f %6.1f  (deg)", detection.ftcPose.pitch, detection.ftcPose.roll, detection.ftcPose.yaw));
+                    telemetryLine(String.format("RBE %6.1f %6.1f %6.1f  (inch, deg, deg)", detection.ftcPose.range, detection.ftcPose.bearing, detection.ftcPose.elevation));
                 } else {
-                    telemetry.addLine(String.format("\n==== (ID %d) Unknown", detection.id));
-                    telemetry.addLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
+                    telemetryLine(String.format("\n==== (ID %d) Unknown", detection.id));
+                    telemetryLine(String.format("Center %6.0f %6.0f   (pixels)", detection.center.x, detection.center.y));
                 }
             }   // end for() loop
     }
@@ -182,12 +183,22 @@ public class AutonomousMode extends LinearOpMode {
         YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
         AngularVelocity angularVelocity = imu.getRobotAngularVelocity(AngleUnit.DEGREES);
 
-        telemetry.addData("Yaw (Z)", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
-        telemetry.addData("Pitch (X)", "%.2f Deg.", orientation.getPitch(AngleUnit.DEGREES));
-        telemetry.addData("Roll (Y)", "%.2f Deg.\n", orientation.getRoll(AngleUnit.DEGREES));
-        telemetry.addData("Yaw (Z) velocity", "%.2f Deg/Sec", angularVelocity.zRotationRate);
-        telemetry.addData("Pitch (X) velocity", "%.2f Deg/Sec", angularVelocity.xRotationRate);
-        telemetry.addData("Roll (Y) velocity", "%.2f Deg/Sec", angularVelocity.yRotationRate);
+        telemetryData("Yaw (Z)", "%.2f Deg. (Heading)", orientation.getYaw(AngleUnit.DEGREES));
+        telemetryData("Pitch (X)", "%.2f Deg.", orientation.getPitch(AngleUnit.DEGREES));
+        telemetryData("Roll (Y)", "%.2f Deg.\n", orientation.getRoll(AngleUnit.DEGREES));
+        telemetryData("Yaw (Z) velocity", "%.2f Deg/Sec", angularVelocity.zRotationRate);
+        telemetryData("Pitch (X) velocity", "%.2f Deg/Sec", angularVelocity.xRotationRate);
+        telemetryData("Roll (Y) velocity", "%.2f Deg/Sec", angularVelocity.yRotationRate);
         telemetry.update();
+    }
+
+    private void telemetryLine(String log) {
+        telemetry.addLine(log);
+        RobotLog.d(log);
+    }
+    private void telemetryData(String log, Object... args) {
+        telemetry.addData(log, args);
+        RobotLog.d(log, args);
+        
     }
 }
